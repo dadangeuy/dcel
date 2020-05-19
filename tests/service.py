3286ea62d51ecc35c7cf2abb188b0c69b7c00caf
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2020-05-19 20:49:38 +0700

    Implement Unit Test for Get Cache

---
+++ tests/service.py
@@ -0,0 +1,36 @@
+from uuid import UUID
+
+from more_itertools import first
+
+
+class StudentService:
+    def __init__(self):
+        self.students = []
+
+    def create(self, id: UUID, name: str) -> dict:
+        student = {
+            'id': id,
+            'name': name
+        }
+        self.students.append(student)
+        return student
+
+    def read(self, id: UUID) -> dict:
+        return first(
+            filter(
+                lambda s: s['id'] == id, self.students
+            )
+        )
+
+    def update(self, id: UUID, name: str) -> dict:
+        student = self.read(id)
+        student['name'] = name
+        return student
+
+    def delete(self, id: UUID) -> dict:
+        student = self.read(id)
+        self.students.remove(student)
+        return student
+
+
+student_service = StudentService()
